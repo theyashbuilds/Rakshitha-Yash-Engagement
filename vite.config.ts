39d9683a0351907215b2d6c3fe4b64d9d0a46260
Yash <theyashbuilds@gmail.com>
2026-04-21 18:56:56 +0530

    Upate

--- vite.config.ts
+++ vite.config.ts
@@ -14,4 +14,12 @@ export default defineConfig(() => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        engagement: path.resolve(__dirname, 'engagement/index.html')
+      }
+    }
+  }
 }));
